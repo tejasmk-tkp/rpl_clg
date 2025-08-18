7434b39d8bd0f15af7595e959ea0f296b41dbf06
tejasmk-tkp <dawn3tkp@gmail.com>
2025-08-18 17:08:56 +0530

    hahaha change

--- 01_rearrange1.py
+++ 01_rearrange1.py
@@ -1,7 +1,7 @@
 import re
 def rearrange_name(name):
 	result = re.search(r"^([\w .-]*), ([\w .-]*)$",name)
-	print('the change')	
+	print('hahaha')	
 	if result==None:
 		return result
 	return "{} {}".format(result[2], result[1])
